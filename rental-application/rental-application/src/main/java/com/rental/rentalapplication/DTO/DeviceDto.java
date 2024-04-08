8c3b01b6e8502e69c79826d9c415ff2c4546ceee
DabrowskaDaria <95188151+DabrowskaDaria@users.noreply.github.com>
2024-04-08 22:11:41 +0200

    Add files via upload

--- rental-application/rental-application/src/main/java/com/rental/rentalapplication/DTO/DeviceDto.java
+++ rental-application/rental-application/src/main/java/com/rental/rentalapplication/DTO/DeviceDto.java
@@ -5,4 +5,28 @@ public class DeviceDto {
 	private int price;
 	private int deposit;
 	private String description;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(int deposit) {
+		this.deposit = deposit;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
